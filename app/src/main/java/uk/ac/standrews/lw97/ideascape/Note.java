55457c3f28b9febbfe37d9d8558266fb5fb5745f
LukeWeidenwalker <lukas.weidenholzer.14@ucl.ac.uk>
2017-11-12 12:58:17 +0000

    Synced notebase and note constructors.

--- app/src/main/java/uk/ac/standrews/lw97/ideascape/Note.java
+++ app/src/main/java/uk/ac/standrews/lw97/ideascape/Note.java
@@ -5,12 +5,16 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class Note extends View {
     String title;
     String content;
     int[] position;
-    int tag;
+    String tag;
     int status;
+    String timestamp;
+    String user;
 
 
     public Note(Context context, AttributeSet attrs) {
@@ -18,9 +22,17 @@ public class Note extends View {
     }
 
 
-    public Note(Context context, AttributeSet attrs, int[] position) {
+    public Note(Context context, AttributeSet attrs, String[] valuesLine) {
         super(context, attrs);
-        this.position = position;
+        this.position = new int[2];
+        this.user = valuesLine[0];
+        this.timestamp = valuesLine[1];
+        this.content = valuesLine[2];
+        this.title = valuesLine[3];
+        this.position[0] = Integer.parseInt(valuesLine[4]);
+        this.position[1] = Integer.parseInt(valuesLine[5]);
+        this.status = Integer.parseInt(valuesLine[6]);
+        this.tag = valuesLine[7];
     }
 
 
@@ -38,6 +50,16 @@ public class Note extends View {
 
     public void setPosition(int[] position) {
         this.position = position;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String[] saveNote() {
+        // Return a full string array containing all attributes of this note
+        return new String[] {this.user, this.timestamp, this.content, this.title, String.valueOf(this.position[0]),
+                String.valueOf(this.position[1]), String.valueOf(this.status), this.tag};
     }
 
     @Override
--- app/src/main/java/uk/ac/standrews/lw97/ideascape/NoteBase.java
+++ app/src/main/java/uk/ac/standrews/lw97/ideascape/NoteBase.java
@@ -1,6 +1,16 @@
 package uk.ac.standrews.lw97.ideascape;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Observable;
 
@@ -10,26 +20,63 @@ public class NoteBase extends Observable {
     private String user;
     private ArrayList<Note> notes;
     // Organise notes into tag groups to allow for more efficient retrieval.
-    private HashMap<Integer, ArrayList<Note>> tagDictionary;
+    private HashMap<String, ArrayList<Note>> tagDictionary;
+    private Context context;
+    private AttributeSet attrs;
 
 
-    NoteBase() {
+    NoteBase(Context context) {
         this.notes = new ArrayList<>();
     }
 
 
-    NoteBase(String user) {
-       this.user = user;
-       this.notes = new ArrayList();
-       loadNotes();
+    NoteBase(Context context, String user) {
+        this.context = context;
+        this.user = user;
+        this.notes = new ArrayList<>();
+        this.tagDictionary = new HashMap<>();
+        loadNotes();
     }
+
 
     public void loadNotes() {
+        // TODO: Improve this so that some characters are escaped: https://stackoverflow.com/questions/769621/dealing-with-commas-in-a-csv-file
+        BufferedReader reader = null;
+        try {
+            File file = new File(this.context.getFilesDir(), this.user + "-notes.csv");
+            if (file.exists()) {
+                reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] valuesLine = line.split(";");
+
+                    if (!(this.tagDictionary.keySet().contains(valuesLine[valuesLine.length-1]))) {
+                        this.tagDictionary.put(valuesLine[valuesLine.length-1], new ArrayList<Note>());
+                        this.tagDictionary.get(valuesLine[valuesLine.length-1]).add(new Note(context, attrs, valuesLine));
+                    }
+
+
+                }
+            }
+
+        }
+        catch (IOException ioe) {
+            Log.e(getTimeStamp(), ioe.getMessage());
+        }
 
     }
 
-    public void saveNotes() {
 
+    public void saveNotes() {
+        // Format (8 entries): USER;TIMESTAMP;CONTENT;TITLE;POSITION_X;POSITION_Y;STATUS;TAG
+    }
+
+    public void saveNotes(String user) {
+
+    }
+
+    private String getTimeStamp() {
+        return new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
     }
 
     public ArrayList<Note> getTagNotes(int tag) {
